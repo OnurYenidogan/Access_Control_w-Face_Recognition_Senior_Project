117606403cc68fb234170be9a4c6bc9fc4eb0fb2
OnurYenidogan <58027395+OnurYenidogan@users.noreply.github.com>
2023-03-17 01:06:45 +0300

    buggy

--- MainDenemeDB.py
+++ MainDenemeDB.py
@@ -2,4 +2,5 @@ from encodeToDB import encode_faces
 # pip install cmake dlib==19.22
 
 if __name__ == '__main__':
-    encode_faces()
+    path = '../Faces'
+    encode_faces(path)
--- encodeToDB.py
+++ encodeToDB.py
@@ -8,55 +8,58 @@ import math
 
 
 
-hostname = 'localhost'
-database = 'SeniorProject'
-username = 'postgres'
-pwd = '1234'
-port_id = 5432
-conn = None
-
-try:
-    with psycopg2.connect(
-                host = hostname,
-                dbname = database,
-                user = username,
-                password = pwd,
-                port = port_id) as conn:
-
-        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-
-            """cur.execute('DROP TABLE IF EXISTS faces')"""
-
-            create_script = ''' CREATE TABLE IF NOT EXISTS faces (
-                                    id      int PRIMARY KEY,
-                                    name    varchar(100) NOT NULL,
-                                    encoding BYTEA '''
-            cur.execute(create_script)
-
-            insert_script  = 'INSERT INTO faces (id, name, encoding) VALUES (%s, %s, %s, %s)'
-            insert_values = [(1, 'James', 12000, 'D1'), (2, 'Robin', 15000, 'D1'), (3, 'Xavier', 20000, 'D2')]
-            for record in insert_values:
-                cur.execute(insert_script, record)
-
-except Exception as error:
-    print(error)
-finally:
-    if conn is not None:
-        conn.close()
-
-
 def encode_faces(path):
     known_face_encodings = []
     known_face_names = []
     for image in os.listdir(path):
         face_image = face_recognition.load_image_file(f"../Faces/{image}")
         face_encodings = face_recognition.face_encodings(face_image)
+        if len(face_encodings) == 0:
+            print("No faces found in the image")
+            return
+
+            # Convert the encoding to a binary representation
+        face_encoding_bytes = np.array(face_encodings[0]).tobytes()
 
         # Skip image if no faces are detected
         if len(face_encodings) == 0:
             print(f"No faces detected in image {image}. Skipping.")
             continue
         face_encoding = face_encodings[0]
+
+        hostname = 'localhost'
+        database = 'SeniorProject'
+        username = 'postgres'
+        pwd = '1234'
+        port_id = 5432
+        conn = None
+
+        try:
+            with psycopg2.connect(
+                    host=hostname,
+                    dbname=database,
+                    user=username,
+                    password=pwd,
+                    port=port_id) as conn:
+
+                with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+                    """cur.execute('DROP TABLE IF EXISTS faces')"""
+
+                    create_script = ''' CREATE TABLE IF NOT EXISTS faces (
+                                                    id SERIAL PRIMARY KEY,
+                                                    name    varchar(100) NOT NULL,
+                                                    encoding BYTEA )'''
+                    cur.execute(create_script)
+
+                    insert_script = 'INSERT INTO faces (name, encoding) VALUES (%s, %s)'
+
+                    cur.execute(insert_script, (image, face_encoding_bytes))
+
+        except Exception as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
 
         known_face_encodings.append(face_encoding)
         known_face_names.append(image)
