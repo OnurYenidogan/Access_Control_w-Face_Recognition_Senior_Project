f9d364db4cdd8ad236950f0b2954c14e938043fc
OnurYenidogan <58027395+OnurYenidogan@users.noreply.github.com>
2023-04-19 07:18:58 +0300

    kamera esnetme çalışıyor ama oran bozuluyor

--- Calisir_Hal_Deneme1/kameraSec.py
+++ Calisir_Hal_Deneme1/kameraSec.py
@@ -1,6 +1,6 @@
+import cv2
 import tkinter as tk
 from tkinter import ttk
-import cv2
 from PIL import Image, ImageTk
 
 
@@ -9,55 +9,61 @@ class KameraSec:
     def __init__(self, root):
         self.root = root
         self.root.title("Kamera Seç")
-        self.root.geometry("640x480")
+        self.root.geometry("600x400")
 
-        self.kameralar = self.list_kameralar()
-        self.kamera_secim = tk.StringVar()
-        self.kamera_secim.set(self.kameralar[0])
-        self.dropdown = ttk.Combobox(self.root, values=self.kameralar, textvariable=self.kamera_secim)
-        self.dropdown.pack(pady=10)
+        self.cap = None
+        self.video_frame = tk.Label(self.root)
+        self.camera_select = ttk.Combobox(self.root, values=self.get_camera_list(), state="readonly")
+        self.camera_select.current(0)
+        self.start_button = tk.Button(self.root, text="Başlat", command=self.start_video)
 
-        self.start_button = tk.Button(self.root, text="Başlat", command=self.baslat)
+        self.camera_select.pack(pady=10)
         self.start_button.pack(pady=10)
+        self.video_frame.pack()
 
-        self.canvas = tk.Canvas(self.root, width=640, height=480)
-        self.canvas.pack()
+        self.root.bind("<Configure>", self.resize_video)
 
-        self.root.bind('<Escape>', lambda e: self.root.quit())  # ESC tuşuna basıldığında çıkış yap
-
-    def list_kameralar(self):
-        kamera_listesi = []
+    def get_camera_list(self):
+        camera_list = []
         for i in range(10):
             cap = cv2.VideoCapture(i)
             if cap.read()[0]:
-                kamera_listesi.append(f"Kamera {i}")
+                camera_list.append(f"Kamera {i}")
             cap.release()
-        return kamera_listesi
+        return camera_list
 
-    def baslat(self):
-        self.dropdown.config(state="disabled")
-        self.start_button.config(state="disabled")
+    def start_video(self):
+        selected_camera_index = int(self.camera_select.get().split()[-1])
+        self.cap = cv2.VideoCapture(selected_camera_index)
+        self.show_video()
 
-        self.cap = cv2.VideoCapture(int(self.kamera_secim.get().split()[-1]))
-        self.show_frame()
-
-    def show_frame(self):
+    def show_video(self):
         ret, frame = self.cap.read()
         if ret:
+            # Resize the frame to fit in the window
+            frame = cv2.resize(frame, (self.root.winfo_width(), self.root.winfo_height()))
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            img = Image.fromarray(frame)
-            img_tk = ImageTk.PhotoImage(img)
-            self.canvas.config(width=self.root.winfo_width(), height=self.root.winfo_height())
-            self.canvas.create_image(0, 0, image=img_tk, anchor='nw')
-            self.canvas.img_tk = img_tk
-            self.root.after(10, self.show_frame)
-        else:
-            self.cap.release()
-            self.dropdown.config(state="normal")
-            self.start_button.config(state="normal")
+            img = tk.PhotoImage(width=frame.shape[1], height=frame.shape[0])
+            img = self.convert_frame_to_photoimage(frame, img)
+            self.video_frame.config(image=img)
+            self.video_frame.image = img
+            self.video_frame.after(10, self.show_video)
 
+    def convert_frame_to_photoimage(self, frame, img):
+        photo = Image.fromarray(frame)
+        photo = ImageTk.PhotoImage(photo)
+        return photo
 
-if __name__ == '__main__':
-    root = tk.Tk()
-    kamera_sec = KameraSec(root)
-    root.mainloop()
+    def resize_video(self, event):
+        if self.cap is not None:
+            _, frame = self.cap.read()
+            frame = cv2.resize(frame, (event.width, event.height))
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = tk.PhotoImage(width=frame.shape[1], height=frame.shape[0])
+            img = self.convert_frame_to_photoimage(frame, img)
+            self.video_frame.config(image=img)
+            self.video_frame.image = img
+
+root = tk.Tk()
+KameraSec(root)
+root.mainloop()
