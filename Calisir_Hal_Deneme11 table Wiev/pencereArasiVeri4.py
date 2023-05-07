84d467c43a16545e2276aaa87da9d5ee431eeebd
OnurYenidogan <58027395+OnurYenidogan@users.noreply.github.com>
2023-05-07 23:57:58 +0300

    kamera seçiminin ilk değeri kendiliğinden geliyor ve kod düzenleme

--- Calisir_Hal_Deneme11 table Wiev/pencereArasiVeri4.py
+++ Calisir_Hal_Deneme11 table Wiev/pencereArasiVeri4.py
@@ -1,18 +1,13 @@
-import cv2
 import tkinter as tk
 from tkinter import ttk
-from PIL import Image, ImageTk
 from some_functions import get_camera_list, get_known_faces_from_db, DBconn
 import subprocess
 import os
 import sys
-import psycopg2
-from tkinter import filedialog
+
 
 global pgConn
 pgConn = DBconn()
-
-
 
 
 class AddFaceWindow:
@@ -28,23 +23,16 @@ class AddFaceWindow:
         )
         self.batch_button.pack()
 
-
-
     def batch_add_faces(self):
         """        folder_path = filedialog.askdirectory()
         print(folder_path)"""
         subprocess.Popen(['python', os.path.join(sys.path[0], 'encodeToDB.py')])
 
 
-
-
-
-
-
 class ShowTableWindow:
     def __init__(self, master):
         self.master = master
-        #self.master.title(f"{db_name} Veritabanı - {table_name} Tablosu")
+        # self.master.title(f"{db_name} Veritabanı - {table_name} Tablosu")
 
         # PostgreSQL veritabanına bağlanma işlemi
         conn = pgConn
@@ -65,7 +53,6 @@ class ShowTableWindow:
         conn.close()
 
 
-
 class CameraSelect:
     def __init__(self, master):
         self.master = master
@@ -84,7 +71,8 @@ class CameraSelect:
         self.typeb_radio.grid(row=1, column=0, padx=5, pady=5)
 
         # Kamera listesi için dropdown
-        self.camera_list = ttk.Combobox(self.master, values=self.BagliCams)
+        self.camera_list = ttk.Combobox(self.master, values=self.BagliCams, state='readonly')
+        self.camera_list.current(0)
         self.camera_list.grid(row=2, column=0, padx=5, pady=5)
 
         # Seçimi onaylamak için buton
@@ -92,16 +80,14 @@ class CameraSelect:
         self.confirm_button.grid(row=3, column=0, padx=5, pady=5)
 
     def cameraStart(self):
-
         print(self.camera_list.get())
         CameraWindowIn_Out.recognation(self, self.camera_list.get(), self.camera_type.get())
-
 
 
 class CameraWindowIn_Out:
     def __init__(self, master, person_list, cam_No):
         self.master = master
-        self.master.title("Cam"+cam_No+" In")
+        self.master.title("Cam" + cam_No + " In")
         self.person_list = person_list
         self.cam_No = cam_No
         print(cam_No)
@@ -135,6 +121,7 @@ def main():
     add_button = tk.Button(root, text="Kişi Ekle", command=lambda: AddFaceWindow(tk.Toplevel()))
     add_button.pack()
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
