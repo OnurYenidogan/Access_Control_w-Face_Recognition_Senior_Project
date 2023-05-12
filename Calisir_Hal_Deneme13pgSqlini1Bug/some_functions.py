0ef27013bd0866de55b3b99661bdbcb812e5a947
OnurYenidogan <58027395+OnurYenidogan@users.noreply.github.com>
2023-05-12 06:01:48 +0300

    VT bilgileri .ini dosyasından okunuyor ve her açılışta kontrol yapılıyor

---
+++ Calisir_Hal_Deneme13pgSqlini1Bug/some_functions.py
@@ -0,0 +1,85 @@
+import cv2
+import psycopg2
+import numpy as np
+
+def get_camera_list():
+    camera_list = []
+    index = 0
+    while True:
+        cap = cv2.VideoCapture(index, cv2.CAP_DSHOW)
+        if not cap.read()[0]:
+            break
+        else:
+            camera_name = index
+            camera_list.append(camera_name)
+        cap.release()
+        index += 1
+    return camera_list
+import configparser
+
+"""def DBconn():
+    config = configparser.ConfigParser()
+    config.read('config.ini')  # ini dosyasını oku
+
+    # İni dosyasından değerleri al
+    hostname = config.get('Database', 'hostname')
+    database = config.get('Database', 'database')
+    username = config.get('Database', 'username')
+    password = config.get('Database', 'password')
+    port_id = config.getint('Database', 'port')
+
+    with psycopg2.connect(
+            host=hostname,
+            dbname=database,
+            user=username,
+            password=password,
+            port=port_id) as conn:
+        return conn"""
+def DBconn():
+    # Veritabanına bağlanma bilgilerini buraya girin
+    hostname = 'localhost'
+    database = 'SeniorProject'
+    username = 'postgres'
+    pwd = '1234'
+    port_id = 5432
+    with psycopg2.connect(
+            host=hostname,
+            dbname=database,
+            user=username,
+            password=pwd,
+            port=port_id) as conn:
+        return conn
+
+def DBconnCheck(hostname, database, username, pwd, port_id):
+    try:
+        with psycopg2.connect(
+                host=hostname,
+                dbname=database,
+                user=username,
+                password=pwd,
+                port=port_id) as conn:
+            return conn
+    except psycopg2.Error as e:
+        print("Veritabanı bağlantısı sağlanamadı:", e)
+        return None
+
+
+def get_known_faces_from_db(conn):
+    known_face_encodings = []
+    known_face_names = []
+
+    with conn.cursor() as cur:
+        # SELECT sorgusu ile 'faces' tablosundaki tüm satırları al
+        cur.execute('SELECT name, encoding FROM faces')
+
+        # Tüm satırları al
+        rows = cur.fetchall()
+        print(rows)
+        # Her satırı döngüde işle
+        for row in rows:
+            name = row[0]
+            encoding_bytes = row[1]
+            encoding = np.frombuffer(encoding_bytes, dtype=np.float64)
+            known_face_encodings.append(encoding)
+            known_face_names.append(name)
+    return known_face_encodings, known_face_names
